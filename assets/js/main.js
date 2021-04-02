b48d890836d99df6ce8a8da49493de52073a7384
Maria Elisa Gomes <mariaelisagmt@gmail.com>
2021-04-02 11:31:43 -0300

    Finalização do menu e efeitos completos do scrolltop

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,9 +13,58 @@ const showMenu = (toggleId, navId) => {
 
 showMenu('nav-toggle', 'nav-menu')
     
-    
 /*====REMOVE MENU MOBILE====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
     //When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')   
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
 /*====SCROLL SECTIONS ACTIVE LINK====*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach( current =>{
+        const sectionHeight = current.offsetHeight + scroll
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*='+sectionId+']').classList.add('active-link')
+        } 
+        else {
+            document.querySelector('.nav__menu a[href*='+sectionId+']').classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
+
 /*====CHANGE BACKGROUND HEADER====*/
-//When the scroll is greater than 200 viewport height, add the scroll-header class
+function scrollHeader() {
+    const nav = document.getElementById('header')
+    //When the scroll is greater than 200 viewport height, add the scroll-header class
+    if (this.scrollY >= 200)
+        nav.classList.add('scroll-header');
+    else
+        nav.classList.remove('scroll-header');
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+/*====SHOW SCROLL TOP====*/
+function scrollTop() {
+    const scrollTop = document.getElementById('scroll-top')
+    if (this.scrollY >= 560)
+        scrollTop.classList.add('scroll-top');
+    else
+        scrollTop.classList.remove('scroll-top');
+}
+
+window.addEventListener('scroll', scrollTop)
